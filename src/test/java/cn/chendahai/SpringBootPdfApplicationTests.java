b9d3ab2ec546510a0c51d77b093d3339d271211c
dahai <1559843332@qq.com>
2020-09-18 10:17:18 +0800

    add itextpdf dependency

---
+++ src/test/java/cn/chendahai/SpringBootPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chendahai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
